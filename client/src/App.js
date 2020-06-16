e8c15f6caaba26d40788cf195064b1e424e69c0f
EgorOnufreychuk <onufreuchuk.egor@rl.odessa.ua>
2020-06-16 23:13:41 +0300

    monuments pages integration

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from './components/Header/';
 import Sidebar from './components/Header/side_bar';
+import Home from './pages/home'
+import About from './pages/about'
+import Login from './pages/login'
+import Monuments from './pages/monuments'
+import Ekaterina from './pages/monuments/Ekaterina'
+import Orange from './pages/monuments/Orange'
+import Duc from './pages/monuments/Duс'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
-import Monument from "./components/monument/index"
+
 class App extends Component {
   state = {
     isOpen: false
@@ -27,27 +34,48 @@ class App extends Component {
   }
 
   render() {
-    // console.log(this.state)
-    // console.log(this.props)
+    console.log(this.state)
     return (
       <Router>
+        <div className="App">
+        <Header
+          submitSearch={this.submitSearch}
+          name="Open Monument Map"
+          btntext="login"
+          openDrawer={this.openDrawer}
+          />
+        <Sidebar
+          closeDrawer={this.closeDrawer}
+          isOpen={this.state.isOpen}
+          onLinkClick={this.closeDrawer}
+        />
+        </div>
         <Switch>
-          <div className="App">
-            <Header
-              submitSearch={this.submitSearch}
-              name="Open Monument Map"
-              btntext="login"
-              openDrawer={this.openDrawer}
-              />
-            <Sidebar
-              closeDrawer={this.closeDrawer}
-              isOpen={this.state.isOpen}
-              />
-            <Route path="/monument/:id" component={Monument} />
-          </div>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/monuments_list">
+            <Monuments />
+          </Route>
+          <Route path="/monuments/ekaterina">
+            <Ekaterina />
+          </Route>
+          <Route path="/monuments/orange">
+            <Orange />
+          </Route>
+          <Route path="/monuments/duc_de_richelie">
+            <Duc />
+          </Route>
         </Switch>
       </Router>
     )
   }
 }
+
 export default App;
--- client/src/components/Header/side_bar.js
+++ client/src/components/Header/side_bar.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import {withStyles} from '@material-ui/core';
 import Drawer from '@material-ui/core/Drawer';
+import HomeIcon from '@material-ui/icons/Home';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import ListItem from '@material-ui/core/ListItem';
-import CodeIcon from '@material-ui/icons/Code';
-import TocIcon from '@material-ui/icons/Toc';
 import {
-  Link,
+  Link
 } from "react-router-dom";
-
 const styles = {
     search: {
 
@@ -16,27 +18,40 @@ const styles = {
 }
 class Sidebar extends Component {
     render() {
+        // const { classes } = this.props;
+      console.log(this.props)
       return (
-          <Drawer anchor={"left"} open={this.props.isOpen} onClose={this.props.closeDrawer}>
-              <nav>
-                <div className="sidebar-navigation">
-                    <ListItem >
-                      <Link to="/monument/information">
-                        <ListItemIcon>{<TocIcon/>}</ListItemIcon>
-                        Information
-                      </Link>
-                    </ListItem>
-                    <ListItem >
-                      <Link to="/monument/bugs_info">
-                        <ListItemIcon>{<CodeIcon/>}</ListItemIcon>
-                        Bugs info
-                      </Link>
-                    </ListItem>
-                </div>
-              </nav>
-          </Drawer>
+        <Drawer anchor={"left"} open={this.props.isOpen} onClose={this.props.closeDrawer}>
+            <div className="sidebar-navigation">
+                <Link to='/home' onClick={this.props.onLinkClick}>
+                  <ListItem button>
+                  <ListItemIcon>{<HomeIcon/>}</ListItemIcon>
+                  <ListItemText primary={'Home'} />
+                  </ListItem>
+                </Link>
+                <Link to='/about' onClick={this.props.onLinkClick}>
+                  <ListItem button>
+                  <ListItemIcon>{<ContactSupportIcon/>}</ListItemIcon>
+                  <ListItemText primary={'About'} />
+                  </ListItem>
+                </Link>
+                <Link to='/login' onClick={this.props.onLinkClick}>
+                  <ListItem button>
+                  <ListItemIcon>{<ExitToAppIcon/>}</ListItemIcon>
+                  <ListItemText primary={'Login'} />
+                  </ListItem>
+                </Link>
+                <Link to='/monuments_list' onClick={this.props.onLinkClick}>
+                  <ListItem button>
+                  <ListItemIcon>{<AccountBalanceIcon/>}</ListItemIcon>
+                  <ListItemText primary={'Monuments'} />
+                  </ListItem>
+                </Link>
+            </div>
+        </Drawer>
       );
     }
   }
+  
 export default withStyles(styles)(Sidebar);
   
